49cb8d33e5988f122e521fc6d79cd2cf295107ee
louis <louis.z.j.li@oocl.com>
2018-07-12 21:39:33 +0800

    [louis] complete the refactor the code  of parkingBoy with mock

--- src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
+++ src/test/java/com/thoughtworks/tdd/ParkingBoyTest.java
@@ -6,16 +6,22 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNot.not;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class ParkingBoyTest {
 
     @Test
     public void should_park_successfully_when_call_boyPark_given_parkingBoy() {
-        ParkingLot parkingLot = new ParkingLot(1);
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        when(parkingLot.isFull()).thenReturn(false);
         ParkingBoy parkingBoy = new ParkingBoy();
         parkingBoy.addParkingLot(parkingLot);
+        Car car = new Car();
         try{
-            parkingBoy.boyPark(new Car());
+            parkingBoy.boyPark(car);
+            verify( parkingLot ).park(car);
         }catch (RuntimeException exception){
             fail("should park successfully");
         }
@@ -34,11 +40,10 @@ public class ParkingBoyTest {
     }
     @Test
     public void should_park_unsuccessfully_when_call_boyPark_given_parkingLot_isFull() {
-        ParkingLot parkingLot = new ParkingLot(1);
-
+        ParkingLot parkingLot = mock(ParkingLot.class);
+        when(parkingLot.isFull()).thenReturn(true);
         ParkingBoy parkingBoy = new ParkingBoy();
         parkingBoy.addParkingLot(parkingLot);
-        parkingBoy.boyPark(new Car());
         try{
             parkingBoy.boyPark(new Car());
             fail("should park successfully");
@@ -48,14 +53,18 @@ public class ParkingBoyTest {
     }
     @Test
     public void should_park_order_given_parking_firt_lot() {
-        ParkingLot parkingLotFirst = new ParkingLot(1);
-        ParkingLot parkingLotSecond = new ParkingLot(1);
         Car car = new Car();
+        ParkingLot parkingLot1 = mock(ParkingLot.class);
+        ParkingLot parkingLot2 = mock(ParkingLot.class);
+
         ParkingBoy parkingBoy = new ParkingBoy();
-        parkingBoy.addParkingLot(parkingLotFirst);
+        parkingBoy.addParkingLot(parkingLot1);
+        parkingBoy.addParkingLot(parkingLot2);
         Receipt receipt = parkingBoy.boyPark(car);
-        assertThat(car, is(parkingLotFirst.unPark( receipt )));
-        assertThat(car, not(parkingLotSecond.unPark( receipt )));
+        when(parkingLot1.unPark(receipt)).thenReturn(car);
+        when(parkingLot2.unPark(receipt)).thenReturn(null);
+        assertThat(car, is(parkingLot1.unPark( receipt )));
+        assertThat(car, not(parkingLot2.unPark( receipt )));
     }
     @Test
     public void should_get_car_success_when_call_boyUnPark_given_rightReceipt() {
